91e00bbad3a3c3dd2b886e5655a0d0b074a2576d
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-09-28 20:44:14 +0100

    data validation with class-validator

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,6 +1,27 @@
+import {
+  IsEmail,
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  IsAlphanumeric,
+} from 'class-validator';
+
 export class AuthDto {
-    email: string
-    password: string
-    firstname: string
-    lastname: string
+  @IsEmail({})
+  @IsNotEmpty()
+  email: string;
+
+  @IsAlphanumeric('en', {
+    message: 'Password should contain number and string only',
+  })
+  @MinLength(8)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstname: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastname: string;
 }
