23ba10c5a2806b1c3fa8b135548cd380a317d340
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-09-28 19:30:09 +0100

    configure to use environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+
+// configs
+// https://dev.to/vjnvisakh/using-env-in-nestjs-3040
+ConfigModule.forRoot()
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
