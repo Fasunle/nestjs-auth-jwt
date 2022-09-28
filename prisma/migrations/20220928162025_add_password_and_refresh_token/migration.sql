4dcfa4138ea8ab1aa19c02bf11b95590c63f876a
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-09-28 17:22:19 +0100

    User should have password and refresh_token

---
+++ prisma/migrations/20220928162025_add_password_and_refresh_token/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `hash_password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "hash_password" TEXT NOT NULL,
+ADD COLUMN     "hash_refresh_token" TEXT;
