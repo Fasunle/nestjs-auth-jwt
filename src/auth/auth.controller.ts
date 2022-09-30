2952df94ef952fba96fe9bc39f86f05cd425e74e
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-09-30 17:05:37 +0100

    Manage User Authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,7 +5,10 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { DisableAccessToken, GetCurrentUser } from 'src/common/decorators';
+import { RefreshTokenGaurd } from 'src/common/gaurds/refresh-token.gaurd';
 import { AuthService } from './auth.service';
 import { AuthDto, LoginAuthDto } from './dto';
 import { TokenType } from './types';
@@ -16,12 +19,14 @@ export class AuthController {
 
   @Post('signup')
   @HttpCode(HttpStatus.CREATED)
+  @DisableAccessToken()
   async signup(@Body() signupInfo: AuthDto) {
     return this.authService.signup(signupInfo);
   }
 
   @Post('login')
   @HttpCode(HttpStatus.OK)
+  @DisableAccessToken()
   async login(@Body() dto: LoginAuthDto): Promise<TokenType> {
     return this.authService.login(dto);
   }
@@ -34,7 +39,12 @@ export class AuthController {
 
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
-  async refresh() {
-    return this.authService.refresh();
+  @DisableAccessToken()
+  @UseGuards(RefreshTokenGaurd)
+  async refresh(
+    @GetCurrentUser('email') email: string,
+    @GetCurrentUser('refreshToken') refreshToken: string,
+  ) {
+    return this.authService.refresh(email, refreshToken);
   }
 }
