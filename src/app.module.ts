efba420a305372f054e1bbe759be50442536a768
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-09-30 16:57:02 +0100

    use AccessTokenGaurd globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGaurd } from './common/gaurds/access-token.gaurd';
 
 // configs
 // https://dev.to/vjnvisakh/using-env-in-nestjs-3040
@@ -12,6 +14,12 @@ ConfigModule.forRoot();
 @Module({
   imports: [PrismaModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGaurd, // use access-token-gaurd aon all routes
+    },
+  ],
 })
 export class AppModule {}
